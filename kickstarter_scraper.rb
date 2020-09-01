1454a429a2fbbc360a138a63adaeb076ebbdf900
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-01 05:25:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,6 +14,7 @@ def create_project_hash
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("span.location-name").text
+  #f
 end
 
 create_project_hash
